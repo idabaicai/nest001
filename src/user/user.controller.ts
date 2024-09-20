c5f332a50ca40121162284b1ac22f27c57069780
liuhui <liuhui@mavericksmobile.com>
2024-09-20 15:24:01 +0800

    feat: create user method

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,6 +1,17 @@
-import { Controller, Get, Query, Req } from '@nestjs/common';
-import { Request } from 'express';
-// import { Response } from '@types/express'
+import {
+  Controller,
+  Get,
+  Param,
+  Post,
+  Query,
+  Redirect,
+  Req,
+  Body,
+  Res,
+  HttpStatus,
+} from '@nestjs/common';
+import { CreateUserDto } from './create-user.dto';
+import { Request, Response } from 'express';
 
 @Controller('user')
 export class UserController {
@@ -8,5 +19,23 @@ export class UserController {
   userList(@Req() req: Request, @Query() params: any): string[] {
     console.log(params, req);
     return ['AAA', 'BBB'];
+  }
+  @Get('all')
+  @Redirect('http://www.qq.com')
+  all() {
+    return 'ok';
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    console.log(id); // user/1 -> 1
+    return 'AAA';
+  }
+
+  @Post('create')
+  create(@Body() user: CreateUserDto, @Res() res: Response) {
+    console.log(user);
+    // return 'ok';
+    res.status(HttpStatus.OK).json({ msg: 'ok', code: 200 });
   }
 }
